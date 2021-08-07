bff529b224c80b3490ab958486973c7203f1b46e
CaedenPH <caedenperelliharris@gmail.com>
2021-08-07 18:19:38 +0100

    Delete HIDDEN.py

--- core/utils/HIDDEN.py
+++
@@ -1,6 +0,0 @@
-TOKEN = 'ODI4MzYzMTcyNzE3MTMzODc0.YGofUg.WhFeyJwtdrmSCcFWsBYoB0Q8UUQ'
-client_id = "GfF3CEfYXsz3yw"
-client_secret = "_gRjhHHlRcb9uWoaJQbJddtqc-E"
-username = "Codex_2006"
-password = "senuka123"
-user_agent = "Codex"
