c9db012718f5e0f75592f8a2c49fe2139110c1de
CaedenPH <caedenperelliharris@gmail.com>
2021-12-27 03:49:01 +0000

    new chatbot cog

---
+++ cogs/Chatbot.py
@@ -0,0 +1,102 @@
+import typing
+import disnake
+import aiosqlite
+
+from disnake.ext import commands
+from core.Context import Context
+
+class ChatBot(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    async def find_or_insert_channel(self, channel_id: int, **kwargs):
+        insert = kwargs.get('insert', False)
+        remove = kwargs.get('remove', False)
+
+        cursor = await self.bot.db.cursor()
+
+        await cursor.execute('Select * from chatbot where channel_id = ?', (channel_id,))
+        result = await cursor.fetchone()
+
+        if not result:
+            if not insert:
+                return False
+            await cursor.execute('Insert into chatbot values (?)', (channel_id,))    
+
+        if remove:
+            await cursor.execute('Delete from chatbot where channel_id = ?', (channel_id,))        
+            
+        return True
+        
+    async def get_response(self, message: str) -> typing.Optional[str]:
+        async with self.bot.client.get(
+                url=f"https://api.pgamerx.com/v5/ai",
+                headers={
+                    'Authorization': 'yFsjaMkrpsKg'
+                    },
+                params={
+                    'message': message,
+                     'server': 'main'
+                    }
+                            ) as resp:
+            json = await resp.json()
+
+        if 'error' in json[0]:
+            return None
+        return json[0]['response']
+
+    async def send_ai(self, channel: Context, message: str, avatar_url=None) -> None:
+        response = await self.get_response(message)
+        
+        embed = disnake.Embed(
+            description=""
+        ).set_author(
+            icon_url=avatar_url or disnake.Embed.Empty,
+            name="Chatbot"
+        )
+        if not response:
+            embed.description = "Something went wrong!"
+            return await channel.send(embed=embed)
+
+        embed.description = f"```yaml\n{response}```"
+        await channel.send(embed=embed)
+
+    @commands.command(aliases=['ai'])
+    async def chatbot(self, ctx: Context, *, message: str) -> None:
+        await self.send_ai(ctx.channel, message)
+
+    @commands.command(aliases=['setup', 'setup_ai'])
+    async def setup_chatbot(self, ctx: Context, channel: disnake.TextChannel) -> None:
+        await ctx.em(f"Setting up my ai capabilites in {channel.name}...")
+        await self.find_or_insert_channel(channel.id, insert=True)
+        await channel.send(
+            embed = disnake.Embed(
+                description="Chatbot is setup!"
+            )
+        )
+        await ctx.em("All set up!")
+    
+    @commands.command(aliases=['remove'])
+    async def remove_ai(self, ctx: Context, channel: disnake.TextChannel) -> None:
+        await ctx.em(f"Removing my ai capabilites in {channel.name}...")
+        await self.find_or_insert_channel(channel.id, remove=True)
+        await channel.send(
+            embed = disnake.Embed(
+                description="Chatbot is removed!"
+            )
+        )
+        await ctx.em("All removed!")
+
+    @commands.Cog.listener('on_message')
+    async def chatbot_on_message(self, message: disnake.Message) -> None:
+        is_channel = await self.find_or_insert_channel(message.channel.id)
+
+        if not is_channel:
+            return
+        if message.author.bot:
+            return
+        await self.send_ai(message.channel, message.content)
+
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(ChatBot(bot))
