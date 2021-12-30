1ab626c04d1d592e27310c2c9f3036bc8c08ce58
CaedenPH <caedenperelliharris@gmail.com>
2021-12-30 00:23:29 +0000

    games

--- cogs/Games.py
+++ cogs/Games.py
@@ -10,6 +10,68 @@ from core.Context import Context
 
 import random
 
+HANGMANPICS = ['', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+     
+  +---+   _   _  _____   ____   ___  _____  ____  
+  |   |  | | | || ____| |  _ \ |_ _|| ____||  _ \ 
+  O   |  | |_| ||  _|   | | | | | | |  _|  | | | |
+ /|\  |  |  _  || |___  | |_| | | | | |___ | |_| |
+ / \  |  |_| |_||_____| |____/ |___||_____||____/ 
+      |
+
+========='''][::-1]
+
+
+                                         
 
 class Card:
         def __init__(self):
@@ -46,9 +108,11 @@ async def buno(ctx, title, description='', **kwargs):
 
 class Games(commands.Cog):
     def __init__(self, bot):
-
         self.bot = bot
     
+    def message_content(self, guesses_left, guesses, word):
+        return f"```yaml\n{HANGMANPICS[guesses_left]}````{' '.join([k if k in guesses else '_' for k in word])}`\nYou have {guesses_left} guesses left"
+
     @commands.command(aliases=['rr'], description="You play russian roulette with yourself - 1 in 6 chance you will die...Be warned!")
     async def russianroulette(self, ctx: Context):
         rand = randint(1, 5)
@@ -586,10 +650,6 @@ class Games(commands.Cog):
             await ctx.send(embed=embed)
     
     @commands.command()
-    async def uno(self, ctx: Context) -> None:
-        await ctx.em("Broken until discord fixes itself...")
-    
-    @commands.command()
     async def anagram(self, ctx: Context) -> None:
         with open('./resources/anagram.json') as stream:
             data = json.load(stream)
@@ -635,33 +695,48 @@ class Games(commands.Cog):
             words = txt.readlines()
 
         word = random.choice(words).strip()
-        print(word)
-        guesses = []
-        guesses_left = 8
+        WORD_WAS = f"The word was `{word}`"
 
-        await ctx.send(f"Your word: `{' '.join(['_' for k in range(0, len(word))])}`\nYou have {guesses_left} guesses left")
+        guesses = []
+        guesses_left = 7
+        
+        msg = await ctx.send(self.message_content(guesses_left, guesses, word))
         try:
             while (m:=await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=60)):
-                if guesses_left == 0:
-                    return await ctx.send(f"Uh oh! You ran out of guesses. The word was {word}")
-                if m.content.lower() == "q":
-                    return await ctx.em("Goodbye")
-
-                if len(m.content.lower()) >= 2 and m.content.lower() != word:
-                    guesses_left -= 1
-                    await ctx.send(f"{m.content.lower()} is not the answer! You have {guesses_left} guesses left")
-                    continue
-
-                guesses.append(m.content.lower())
-                if m.content.lower() == word:
-                    return await ctx.em(f"Well done! You got the word. The word was `{word}`")
-
+                """
+                5 Conditions in order;
+                    a) content == q; exit
+                    b) content == word; win
+                    c) all letters guessed are in word; win
+                    d) guesses_left == 1; loss 
+                    e) content > 1 letter; punt
+                then; 
+                    if content not in guesses;
+                        if content not in word;
+                            guesses_left -= 1
+                        then;
+                        -> edit(...)
+                """
+            
+                content = m.content.lower()
+                guesses.append(content)
+                
+                if content == "q":
+                    return await ctx.em(f"Game ended. {WORD_WAS}")
+                if content == word:
+                    return await ctx.em(f"That is the word! {WORD_WAS}")
                 if all([w in guesses for w in list(word)]):
-                    return await ctx.em(f"Well done! You got the word. The word was `{word}`")
+                    return await ctx.em(f"Well done! You got the word. {WORD_WAS}")
+                if guesses_left == 1:
+                    return await ctx.em(f"Unlucky, you ran out of guesses! {WORD_WAS}")
+                if len(content) >= 2:
+                    await ctx.em(f"`{content}` is not the word! Try sending letters one at a time")
 
-                if m.content.lower() not in word:
-                    guesses_left -= 1
-                await ctx.send(f"word: `{''.join([k if k in guesses else ' _' for k in word])}`\nYou have {guesses_left} guesses left")
+                if content not in guesses[:-1]:
+                    if content not in word:
+                        guesses_left -= 1
+
+                await msg.edit(content=self.message_content(guesses_left, guesses, word))
 
         except asyncio.TimeoutError:
             await ctx.em("You ran out of time!")
